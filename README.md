ec049d80f2d7b0d9d305b86fc40cbdc233c072af
hunnx27 <hunnx27@i-screammedia.com>
2025-12-30 17:22:19 +0900

    작업중

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+# Spring integration MQTT DOC
+https://docs.spring.io/spring-integration/reference/mqtt.html
+- 메이븐 디펜던시 입력으로 라이브러리 다운로드
+- 자바 설정
+  - 인바운드(메시지 기반) 채널 어댑터 > Java 구성을 사용한 구성
+
+https://mvnrepository.com/artifact/org.eclipse.paho/org.eclipse.paho.mqttv5.client
--- src/main/java/com/kongju/middleware/KMiddlewareApplication.java
+++ src/main/java/com/kongju/middleware/KMiddlewareApplication.java
@@ -1,13 +1,59 @@
 package com.kongju.middleware;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 
 @SpringBootApplication
 public class KMiddlewareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KMiddlewareApplication.class, args);
+        new SpringApplicationBuilder(KMiddlewareApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+//		SpringApplication.run(KMiddlewareApplication.class, args);
 	}
+
+
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+    @Bean
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter("tcp://localhost:1883", "testClient",
+                        "topic1", "topic2");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                System.out.println(message.getPayload());
+            }
+
+        };
+    }
 
 }
