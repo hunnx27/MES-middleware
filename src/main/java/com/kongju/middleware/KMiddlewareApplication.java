5d1e48163761b77252791157c24033f9ed19d942
hunnx27 <hunnx27@i-screammedia.com>
2025-12-30 16:31:19 +0900

    feat: init
    최초 프로젝트 셋팅

---
+++ src/main/java/com/kongju/middleware/KMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.kongju.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KMiddlewareApplication.class, args);
+	}
+
+}
